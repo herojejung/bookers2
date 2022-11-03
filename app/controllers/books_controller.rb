a6fa9fed5d10d84f1502d9dded2871efb7f3d8f8
YUTA KATSUBE <herojejung@users.noreply.github.com>
2022-11-03 12:42:52 +0000

    [update]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,19 +5,19 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
+    book.user_id = current_user.id
     book.save
-    redirect_to books_path(@book.id)
+    redirect_to book_path(book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
     @user = current_user
   end
 
   def show
-  end
-
-  def create
+    @book = Book.find(params[:id])
   end
 
   private
